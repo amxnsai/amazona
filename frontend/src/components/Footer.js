b7881cba9027516685b4cc70ebcfb7f19168660e
= <=>
2020-11-06 12:01:03 +0100

    React Header

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container,Row,Col} from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            footer
+        </footer>
+    )
+}
+
+export default Footer
